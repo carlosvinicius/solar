0a15616ef3c61c92bb965d51b29c22cc4b4a3812
carlosvinicius <carlos@cvps.com.br>
2018-11-21 19:20:40 -0200

    one day electricity updated at the controller

--- app/Http/Controllers/OneDayElectricityController.php
+++ app/Http/Controllers/OneDayElectricityController.php
@@ -2,27 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
+use \DateTime;
+use App\Models\OneDayElectricity;
 use App\Models\Panel;
+use Illuminate\Http\Request;
 
 class OneDayElectricityController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function index(Request $request)
     {
-        $panel = Panel::where('serial', $request->panel_serial)->first();
+        $panel     = Panel::where('serial', $request->panel_serial)->firstOrFail();
+        $yesterday = new DateTime('yesterday');
 
-        return [[
-            'day' => null,
-            'sum' => 0,
-            'min' => 0,
-            'max' => 0,
-            'average' => 0
-        ]];
+        return OneDayElectricity
+                ::where('panel_id', $panel->id)
+                ->where('day', $yesterday->format('Y-m-d'))
+                ->select(['day', 'sum', 'min', 'max','average'])
+                ->get();
     }
 }
