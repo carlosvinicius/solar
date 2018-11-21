1b548b10c9380320d2ef9c93c65854eb40ee00b9
carlosvinicius <carlos@cvps.com.br>
2018-11-21 15:53:19 -0200

    bugs fixed to attend business criteria

--- app/Models/OneHourElectricity.php
+++ app/Models/OneHourElectricity.php
@@ -9,8 +9,8 @@ class OneHourElectricity extends Model
     protected $fillable = ['panel_id', 'kilowatts', 'hour'];
 
     public static $fieldValidations = [
-        'banel_id'  => 'required',
+        'panel_id'  => 'required',
         'kilowatts' => 'required',
-        'hour'      => 'required|date_format:Y-m-d H:i:s|unique:one_hour_electricities,hour,NULL,panel_id'
+        'hour'      => 'required|date_format:Y-m-d H:00:00|unique:one_hour_electricities,hour,NULL,panel_id'
     ];
 }
